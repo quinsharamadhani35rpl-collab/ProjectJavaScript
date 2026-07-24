9d74f3c458a54a2936e123e385f0ae336e81b2eb
quinsharamadhani35rpl-collab <quinsha_ramadhani_35rpl@student.smktelkom-mlg.sch.id>
2026-07-24 07:50:36 +0700

    Add files via upload

---
+++ quin.js
@@ -0,0 +1,4 @@
+console.log("Quinsha Ramadhani");
+console.log("28 Agustus 2010");
+console.log("SMPN 24 Malang");
+console.log("Alasan saya memilih Jurusan RPL karena saya ingin menjadi programmer atau data analyst")
